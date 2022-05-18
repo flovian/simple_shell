b90f511936f2bb93dfef9c7a9e6543034fe53913
victorazangu <victorsazangu@gmail.com>
2022-05-18 17:06:31 +0300

    env_func

--- built.c
+++ built.c
@@ -42,19 +42,35 @@ int get_built(list_t *input_list, char *shell_name, list_t *env_list)
  */
 int exit_shell(list_t *input_list, char *shell_name, list_t **env_list_ptr)
 {
-	int num;
+    int num;
 
-	if (input_list->next == NULL)
-		return (0);
+    if (input_list->next == NULL)
+        return (0);
 
-	num = _atoi(input_list->next->name);
+    num = _atoi(input_list->next->name);
 
-	if (num == -1)
-	{
-		print_error(shell_name, "Invalid exit status\n");
-		return (-2);
-	}
+    if (num == -1)
+    {
+        print_error(shell_name, "Invalid exit status\n");
+        return (-2);
+    }
 
-	(void)env_list_ptr;
-	return (num);
+    (void)env_list_ptr;
+    return (num);
+}
+/**
+ * env_func - implements the env built-in functionality
+ * @input_list: list of command and arguments
+ * @shell_name: name of shell program
+ * @env_list_ptr: pointer list of environment variables
+ *
+ * Return: -2
+ */
+int env_func(list_t *input_list, char *shell_name, list_t **env_list_ptr)
+{
+    print_env(*env_list_ptr);
+
+    (void)input_list;
+    (void)shell_name;
+    return (-2);
 }
